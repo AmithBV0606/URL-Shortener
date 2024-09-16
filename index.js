1c24329e8c0f8155d5d39267c2709c5484d60866
AmithBV0606 <amithrao0606@gmail.com>
2024-09-16 23:52:12 +0530

    Added stateless authentication.

--- index.js
+++ index.js
@@ -2,25 +2,35 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
-const { connectToMongoDB } = require("./connect");
+const cookieParser = require("cookie-parser");
+const { restrictToLoggedinUserOnly, checkAuth } = require("./middleware/auth")
+
+// Routes
 const urlRoute = require("./routes/url");
+const staticsRoute = require("./routes/staticRouter");
+const userRouter = require("./routes/user");
+
 
 const path = require("path");
-const staticsRoute = require("./routes/staticRouter");
 
 // EJS setup
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
+// Database connection
+const { connectToMongoDB } = require("./connect");
 connectToMongoDB("mongodb://127.0.0.1:27017/short-url").then(() =>
   console.log("Database connected")
 );
 
+// Common middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false })); // Middleware to parse form data
+app.use(cookieParser());
 
-app.use("/url", urlRoute);
+// Route handling middlewares
+app.use("/url", restrictToLoggedinUserOnly, urlRoute);
+app.use("/user", userRouter);
+app.use("/", checkAuth, staticsRoute);
 
-app.use("/", staticsRoute);
-
-app.listen(PORT, () => console.log(`Server started on port : ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port : ${PORT}`)); 
